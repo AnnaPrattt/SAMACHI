62b37c3d3d35fa327c7ebee449b61467e1b3b973
AnnaPrattt <annapratt6@gmail.com>
2023-09-26 19:31:40 -0600

    Changed conditionals to check object properties instead of strings.

--- Automated_Tests.ps1
+++ Automated_Tests.ps1
@@ -6,26 +6,16 @@ echo "--------------------"
 #Gather firewall status
 $result = Get-NetFirewallProfile
 #$result[0] is the results only for the DOMAIN firewall
-$contentForDomainFW = Write-Output $result[0]
-#Store the information about the DOMAIN firewall in a text file
-#This is necessary because only strings can use the Select-String command later
-Write-Output $contentforDomainFW  | out-file -filepath firewallDomainTest.txt
-#Output only the line about if the domain firewall is enabled
-$isDomainEnabled = Get-Content firewallDomainTest.txt | Select-String "Enabled"
-if ($isDomainEnabled -like "*True*") {
+$contentForDomainFW = $result[0]
+if ($contentForDomainFW.enabled -eq $true) {
     echo "Your domain firewall is enabled. Way to be secure!"
 }
 else {
     echo "Your domain firewall is disabled. You should turn it on to protect your computer."
 }
 
-$contentForPrivateFW = Write-Output $result[1]
-#Store the information about the PRIVATE firewall in a text file
-#This is necessary because only strings can use the Select-String command later
-Write-Output $contentforPrivateFW  | out-file -filepath firewallPrivateTest.txt
-#Output only the line about if the PRIVATE firewall is enabled
-$isPrivateEnabled = Get-Content firewallPrivateTest.txt | Select-String "Enabled"
-if ($isPrivateEnabled -like "*True*") {
+$contentForPrivateFW = $result[1]
+if ($contentForPrivateFW.enabled -eq $true) {
     echo "Your private firewall is enabled. Way to be secure!"
 }
 else {
@@ -33,12 +23,7 @@ else {
 }
 
 $contentForPublicFW = Write-Output $result[2]
-#Store the information about the PUBLIC firewall in a text file
-#This is necessary because only strings can use the Select-String command later
-Write-Output $contentforPublicFW  | out-file -filepath firewallPublicTest.txt
-#Output only the line about if the PUBLIC firewall is enabled
-$isPublicEnabled = Get-Content firewallPublicTest.txt | Select-String "Enabled"
-if ($isPublicEnabled -like "*True*") {
+if ($contentForPublicFW.enabled -eq $true) {
     echo "Your public firewall is enabled. Way to be secure!"
 }
 else {
@@ -54,20 +39,13 @@ echo "--------------------"
 $guestUserContent = Get-LocalUser -Name Guest
 #Store the information about the guest user in a text file
 #This is necessary because only strings can be compared in if/else statements later
-Write-Output $guestUserContent  | out-file -filepath guestUserInformation.txt
-$guestUserInformation = Get-Content guestUserInformation.txt
-if ($guestUserInformation -like "*False*") {
+if ($guestUserContent.enabled -eq $false) {
     echo "Your Guest account is disabled. Way to be secure!"
 }
 else {
     echo "Your Guest account is enabled. You should disable it to be secure."
 }
 
-#Clean up files
-Remove-Item firewallDomainTest.txt
-Remove-Item firewallPrivateTest.txt
-Remove-Item guestUserInformation.txt
-Remove-Item firewallPublicTest.txt
 
 
 echo ""

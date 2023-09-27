fedf2ed8d92a7cc65c55923f0900fb41556640ad
AnnaPrattt <annapratt6@gmail.com>
2023-09-26 19:57:45 -0600

    Updated comments in code.

--- Automated_Tests.ps1
+++ Automated_Tests.ps1
@@ -3,8 +3,11 @@ echo "Welcome to the automated security audit. These findings will show you if y
 echo ""
 echo "Firewall:"
 echo "--------------------"
-#Gather firewall status
+#Gather firewall status into one variable
+#This result will later be parsed into its different firewall profiles
 $result = Get-NetFirewallProfile
+
+
 #$result[0] is the results only for the DOMAIN firewall
 $contentForDomainFW = $result[0]
 if ($contentForDomainFW.enabled -eq $true) {
@@ -14,6 +17,7 @@ else {
     echo "Your domain firewall is disabled. You should turn it on to protect your computer."
 }
 
+#$result[0] is the results only for the PRIVATE firewall
 $contentForPrivateFW = $result[1]
 if ($contentForPrivateFW.enabled -eq $true) {
     echo "Your private firewall is enabled. Way to be secure!"
@@ -22,6 +26,7 @@ else {
     echo "Your private firewall is disabled. You should turn it on to protect your computer."
 }
 
+#$result[1] is the results only for the PUBLIC firewall
 $contentForPublicFW = Write-Output $result[2]
 if ($contentForPublicFW.enabled -eq $true) {
     echo "Your public firewall is enabled. Way to be secure!"
@@ -30,15 +35,11 @@ else {
     echo "Your public firewall is disabled. You should turn it on to protect your computer."
 }
 
-
-
 echo ""
 echo "User Accounts:"
 echo "--------------------"
 
 $guestUserContent = Get-LocalUser -Name Guest
-#Store the information about the guest user in a text file
-#This is necessary because only strings can be compared in if/else statements later
 if ($guestUserContent.enabled -eq $false) {
     echo "Your Guest account is disabled. Way to be secure!"
 }
@@ -47,21 +48,17 @@ else {
 }
 
 
-
 echo ""
 echo "Windows Updates:"
 echo "--------------------"
-#I found this command online at https://www.action1.com/check-missing-windows-updates-script/
+#Command sourced from https://www.action1.com/check-missing-windows-updates-script/
 #Show Windows updates that still need to be installed
 (New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search('IsHidden=0 and IsInstalled=0').Updates | Select-Object Title
 
 # Sleep for 5 seconds. This presents the output of the Windows Updates
 # section from being cut off by the following commands
+Start-Sleep -Seconds 3
 
 echo ""
-Start-Sleep -Seconds 3
 echo "Testing complete."
 Read-Host -Prompt "Press Enter to exit"
-
-
-
